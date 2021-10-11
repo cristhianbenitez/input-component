aaab7c8fe1126e2723ab1af01c673845f451016c
Cristhian Benitez <cristhiantilleria@gmail.com>
2021-10-11 16:26:37 -0400

    I can choose to disable input

--- src/App.js
+++ src/App.js
@@ -32,12 +32,38 @@ function App() {
           <div className="input-container">
             <p className="grey-text">{'&:hover'}</p>
 
-            <Input />
+            <Input error />
           </div>
           <div className="input-container">
             <p className="grey-text">{'&:focus'}</p>
 
             <Input error />
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-container">
+            <p className="regular-text">{'<Input disabled />'}</p>
+
+            <Input disabled />
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-container">
+            <p className="regular-text">{'<Input value="text" />'}</p>
+
+            <Input value="text" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-container">
+            <p className="regular-text">{'<Input size="sm" />'}</p>
+
+            <Input size="sm" />
+          </div>
+          <div className="input-container">
+            <p className="regular-text">{'<Input size="md" />'}</p>
+
+            <Input size="md" />
           </div>
         </div>
       </div>
--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './input.css';
 
-const Input = ({ error = false, ...restProps }) => {
+const Input = ({
+  error = false,
+  disabled = false,
+  size = 'md',
+  ...restProps
+}) => {
   const checkError = error ? 'error' : '';
-
+  const checkDisabled = disabled ? 'disabled' : '';
   return (
-    <label className={` ${checkError}`}>
+    <label>
       Label
-      <div className={`input-element ${checkError}`}>
-        <input type="text" placeholder={'Placeholder'} {...restProps} />
+      <div className={`input-element ${checkError} ${checkDisabled} ${size}`}>
+        <input
+          type="text"
+          placeholder={'Placeholder'}
+          disabled={disabled}
+          {...restProps}
+        />
       </div>
     </label>
   );
